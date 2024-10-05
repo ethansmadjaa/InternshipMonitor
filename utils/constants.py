8a6639b2f5fd5998345cfe6674b7f1b23e77df06
ethansmadjaa <ethansmadja@icloud.com>
2024-10-05 16:34:44 +0200

    📝 Add CSV_HEADERS list to constants.py for candidate data

--- utils/constants.py
+++ utils/constants.py
@@ -3,3 +3,15 @@
 CSV_FILE = 'candidatures.csv'
 CSV_SEPARATOR = ';'
 CSV_ENCODING = 'utf-8'
+
+CSV_HEADERS = [
+    'Entreprise',
+    'Intitulé',
+    'Date',
+    'Statut',
+    'Détails',
+    'Canal',
+    'Nom du contact',
+    'Email du contact',
+    'Téléphone du contact',
+]
